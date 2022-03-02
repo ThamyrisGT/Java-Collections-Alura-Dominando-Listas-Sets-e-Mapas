f7823f5ad072fefaa9e220c0dafa2a9c130fd564
Thamyris <tgthurler@gmail.com>
2022-03-02 10:18:49 -0300

    Aplicando Set no modelo

--- gerenciador-de-cursos/src/br/com/alura/curso/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/curso/Curso.java
@@ -1,14 +1,17 @@
 package br.com.alura.curso;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private Integer tempoTotal;
 	private String nome;
 	private String instrutor;
+	private Set<Aluno> alunos = new HashSet<>();
 	// sempre mais vantajoso usar o List do lado esquerdo, sem especificar o tipo da
 	// lista, isso facilita para manutenção do código,
 	// e também para deixar o código mais "escondido" - codar na defensiva
@@ -39,6 +42,10 @@ public class Curso {
 		return instrutor;
 	}
 
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+
 //	public List<Aula> getAulas() {
 //		return aulas;
 //	}
@@ -62,8 +69,12 @@ public class Curso {
 
 	@Override
 	public String toString() {
-        return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal()
-        + ", aulas: + " + this.aulas + "]";
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: + " + this.aulas
+				+ "]";
 	}
-	
+
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
+	}
+
 }
